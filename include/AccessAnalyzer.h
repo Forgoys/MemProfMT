545b1e92e8057d730ebffffe11c26f63ce163d03
Huaijin Li <578761491@qq.com>
2025-02-15 16:52:45 +0800

    MemoryInstrumentation into the function now works

--- include/AccessAnalyzer.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by 李木头 on 25-1-3.
-//
-
-#ifndef ACCESSANALYZER_H
-#define ACCESSANALYZER_H
-
-#endif //ACCESSANALYZER_H
---
+++ runtime/MemoryProfiler.h
@@ -0,0 +1,8 @@
+//
+// Created by 李木头 on 25-1-4.
+//
+
+#ifndef MEMORYPROFILER_H
+#define MEMORYPROFILER_H
+
+#endif //MEMORYPROFILER_H
